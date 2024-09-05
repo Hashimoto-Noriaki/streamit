b9ded8bca8c35b1297dc632f4f46441523e0380d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-09-05 19:34:21 +0900

    チャートをcheckboxで表示非表示をできるようにする

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ st.title('My 1st app')
 st.write('データ')
 st.write(
     pd.DataFrame({
-        '1st columin':[1,2,3,4],
-        '2st columin':[10,20,30,40],
+        '1st columin': [1, 2, 3, 4],
+        '2st columin': [10, 20, 30, 40],
     })
 )
-chart_df = pd.DataFrame(
-    np.random.randn(20,3),
-    columns = ['a','b','c']
-)
-st.line_chart(chart_df)
+
+# チェックボックスがオンの場合のみ DataFrame を表示
+if st.checkbox('Show DataFrame'):
+    chart_df = pd.DataFrame(
+        np.random.randn(20, 3),
+        columns=['a', 'b', 'c']
+    )
+    st.line_chart(chart_df)
